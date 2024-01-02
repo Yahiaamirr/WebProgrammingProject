43427459736adbb79392f48d47cafaec02196c13
alialaa1240 <155433538+alialaa1240@users.noreply.github.com>
2024-01-02 11:37:53 +0200

    login

--- login.php
+++ login.php
@@ -1,38 +1,33 @@
+<html>
+<link rel="stylesheet" href="style.css">
+<body>
+<header>
+<blockquote>
+    <a href="index.php"><img src="image/logo.png"></a>
+</blockquote>
+</header>
+<blockquote>
+<div class="container">
+<center><h1>Login</h1></center>
+<form action="checklogin.php" method="post">
+    Username:<br><input type="text" name="username"/>
+    <br><br>
+    Password:<br><input type="password" name="pwd" />
+    <br><br>
+    <input class="button" type="submit" value="Login"/>
+    <input class="button" type="button" name="cancel" value="Cancel" onClick="window.location='index.php';" />
+</form>
+</div>
+<blockquote>
 <?php
-include("conn.php");
-session_start();
-
-$error = "";
-$name = $_POST["name"];
-$password = $_POST["password"];
-
-// Use prepared statements to prevent SQL injection
-$stmt = $con->prepare("SELECT id, username, password FROM users WHERE username = ?");
-$stmt->bind_param("s", $name);
-$stmt->execute();
-$result = $stmt->get_result();
-
-if ($result->num_rows > 0) {
-    $arr = $result->fetch_assoc();
-
-    // Verify password using password_verify
-    if (password_verify($password, $arr['password'])) {
-        $_SESSION['id'] = $arr['id'];
-        $_SESSION['username'] = $arr['username'];
-
-        // Redirect to a welcome page using JavaScript
-        echo '<script>';
-        echo 'alert("Welcome back, ' . $arr['username'] . '!");';
-        echo 'window.location.href = "myProject.html";'; // Replace "welcome.php" with your actual welcome page
-        echo '</script>';
-        exit();
-    } else {
-        $error = "Invalid username or password.";
+if(isset($_GET['errcode'])){
+    if($_GET['errcode']==1){
+        echo '<span style="color: red;">Invalid username or password.</span>';
+    }elseif($_GET['errcode']==2){
+        echo '<span style="color: red;">Please login.</span>';
     }
-} else {
-    $error = "Invalid username or password.";
 }
 
-echo $error;
 ?>
-
+</body>
+</html>
